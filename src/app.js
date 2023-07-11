6618bf4dcb3a27837e5e1e349bfab820ec65a1ae
Pedro Luis Gutierrez <83560861+iampedroluis@users.noreply.github.com>
2023-07-11 04:55:02 +0000

    upload code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+
+function dominio() {
+  let dominiosgen = [];
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+      for (var k = 0; k < noun.length; k++) {
+        let dominiosr = pronoun[i] + adj[j] + noun[k] + ".com";
+        dominiosgen.push(dominiosr);
+      }
+    }
+  }
+  return dominiosgen;
+}
+
+let data = dominio();
+
+console.log(data);
+
+for (var i = 0; i < data.length; i++) {
+  let li = document.createElement("li");
+  let elemetn = document.getElementById("lista").appendChild(li);
+  elemetn.classList.add("list-group-item");
+  elemetn.innerHTML = data[i];
+}
